5ccba605c870433af527b52e1be0a8c50784659a
tibawatanabe <tibawatanabe@gmail.com>
2018-05-17 11:52:21 -0300

    Add support for jsx extentions

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { AuthenticationHttpDataSource as dataSource } from './data/http';
+
 export default class App extends React.Component {
   render() {
+    dataSource.login('admin@taqtile.com', '1111', false)
+              .then(data => console.log(data));
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
